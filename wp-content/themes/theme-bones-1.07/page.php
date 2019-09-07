5af56e0be0cc1a2606f33acff19106428bdfddca
cp <chris@cpsharp.net>
2019-09-07 19:28:28 -0400

    first commit

---
+++ wp-content/themes/theme-bones-1.07/page.php
@@ -0,0 +1,69 @@
+<?php get_header(); ?>
+
+			<section id="content">
+
+				<section id="pg" style="display: block; opacity: 1">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<header class="article-header">
+
+									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+
+								</header> <?php // end article header ?>
+
+								<section class="entry-content cf" itemprop="articleBody">
+									<?php
+										// the content (pretty self explanatory huh)
+										the_content();
+
+										/*
+										 * Link Pages is used in case you have posts that are set to break into
+										 * multiple pages. You can remove this if you don't plan on doing that.
+										 *
+										 * Also, breaking content up into multiple pages is a horrible experience,
+										 * so don't do it. While there are SOME edge cases where this is useful, it's
+										 * mostly used for people to get more ad views. It's up to you but if you want
+										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
+										 *
+										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
+										 *
+										*/
+										wp_link_pages( array(
+											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
+											'after'       => '</div>',
+											'link_before' => '<span>',
+											'link_after'  => '</span>',
+										) );
+									?>
+								</section> <?php // end article section ?>
+
+								<footer class="article-footer cf">
+
+								</footer>
+
+								<?php comments_template(); ?>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+
+						<?php // get_sidebar(); ?>
+
+				</section>
+
+                <aside id="aside">
+                    <div id="aside_slideshow">
+                        <div class="block slideshow block-alt">
+                            <div class="photo owl-carousel" style="display: none"></div>
+                            <h3 class="empty-text" style="display: block">Photos coming soon.</h3>
+                        </div>
+                    </div>
+                </aside>
+
+			</section>
+
+<?php // get_footer(); ?>
